53aa4a9c65e861f4ebf761ce0edcc33fe31d8383
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-02-19 19:26:34 -0600

    se quito codigo basura

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,174 +1,208 @@
 import React, { useEffect, useState } from 'react';
-import '../styles/styles.css';
+
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+
 const Calendar = () => {
 
-  const [date, setDate] = useState(new Date());
-  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [currDate, setCurrDate] = useState(new Date());
+    const [events, setEvents] = useState([]);
+    const [ personalEvents, setPersonalEvents ] = useState([]);
 
-  useEffect(() => {
-    // let date = new Date();
-    // let currYear = date.getFullYear();
-    // let currMonth = date.getMonth();
+    let currMonth = currDate.getMonth();
+    let currYear = currDate.getFullYear();
+    console.log(events);
+    console.log(localStorage.getItem('event'));
 
-    // renderCalendar();
-    console.log(date);
-    console.log(currentMonth);
-    console.log(currentYear);
-  }, []);
+    useEffect(() => {
 
-  const renderCalendar = () => {
-    let currYear = date.getFullYear();
-    let currMonth = date.getMonth();
-    let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(); // días que tiene el mes
-    let lastDateOfPrevMonth = new Date(currYear, currMonth, 0).getDate(); // días que tiene el mes anterior
-    let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(); // día de la semana que inicia el mes (0-6)
-    let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay(); // último día de la semana del mes anterior
+        getEventsData();
 
-    let tdTag = [];
+    }, []);
 
-    // ciclo para los últimos días del mes previo
-    for(let i = firstDayOfMonth; i > 0; i--) {
-      // tdTag.push(<td key={`${currMonth} ${i}`} className="inactive">{lastDateOfPrevMonth - i + 1}</td>);
-      tdTag.push([ { 
-        key: `${months[currMonth-1]} ${lastDateOfPrevMonth - i + 1}`, 
-        className: "inactive", 
-        value: lastDateOfPrevMonth - i + 1
-      } ]);
-    }
-
-    // // ciclo para los días del mes
-    for(let i = 1; i <= lastDateOfMonth; i++) {
-      // revisar si es el día actual
-      let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? 'active' : '';
-      // tdTag.push(<td key={`${currMonth} ${i}`} className={isToday}>{i}</td>);
-      tdTag.push([ { 
-        key: `${months[currMonth]} ${i}`, 
-        className: isToday, 
-        value: i
-      } ]);
-    }
-
-    // // ciclo para los primeros días del siguiente mes
-    for(let i = lastDayOfMonth; i < 6; i++) {
-      // tdTag.push(<td key={`${currMonth} ${i}`} className="inactive">{i - lastDayOfMonth + 1}</td>);
-      tdTag.push([ { 
-        key: `${months[currMonth+1]} ${i - lastDayOfMonth + 1}`, 
-        className: "inactive", 
-        value: i - lastDayOfMonth + 1
-      } ]);
-    }
-
-    const separatedArrays = [];
+    const getPersonalEvents = () => {
+        if(localStorage.getItem('event')) {
+            const eventsFromStorage = JSON.parse(localStorage.getItem('event'));
+            console.log(eventsFromStorage);
+            setEvents(events => [...events, ...eventsFromStorage]);
+            localStorage.removeItem('event');
+        }
+    };
     
-    while (tdTag.length > 0) {
-      separatedArrays.push(tdTag.splice(0, 7));
-    }
+    const getEventsData = async () => {
+        const response = await fetch('https://altomobile.blob.core.windows.net/api/test.json')
+        const data = await response.json();
 
-    const daysOfMonth = separatedArrays.map((array, index) => (
-      <tr className='row' key={index}>
-        {array.map((elemento, i) => (
-          // console.log(elemento[0], i)
-          <td
-            key={ elemento[0].key } 
-            className={ `col ${elemento[0].className}` }
-            onClick={handleDay}
-          >
-            { elemento[0].value }
-          </td>
-        ))}
-      </tr>
-    ));
+        const eventsArray = data.map(item => {
+            let date = new Date(item.time);
 
-    return daysOfMonth;
+            return {
+                date: `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`,
+                // time: `${date.getHours() + 6}:${date.getMinutes()}`,
+                eventName: item.name
+            };
+        });
+        console.log(eventsArray);
+        setEvents([...events, ...eventsArray]);
+    };
 
 
+    const renderCalendar = () => {
+        let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(); // días que tiene el mes
+        let lastDateOfPrevMonth = new Date(currYear, currMonth, 0).getDate(); // días que tiene el mes anterior
+        let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(); // día de la semana que inicia el mes (0-6)
+        let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay(); // último día de la semana del mes anterior
 
-    // currentDate.innerHTML = `${months[date.getMonth()]} ${date.getFullYear()}`;
-    // daysTag.innerHTML = liTag;
-  };
+        let tdTag = [];
 
+        // ciclo para los últimos días del mes previo
+        for(let i = firstDayOfMonth; i > 0; i--) {
+            tdTag.push([ { 
+                // key: `${months[currMonth-1]} ${lastDateOfPrevMonth - i + 1}`, 
+                key : `${lastDateOfPrevMonth - i + 1} ${currMonth} ${currYear}`,
+                className: "inactive", 
+                value: lastDateOfPrevMonth - i + 1
+            } ]);
+        }
 
-  const handlePrevNext = (event) => {
-    // console.log(event.target.id);
-    if(event.target.id === "prev") {
-      setCurrentMonth( currentMonth - 1);
-    } else {
-      setCurrentMonth( currentMonth + 1);
-    }
+        // ciclo para los días del mes
+        for(let i = 1; i <= lastDateOfMonth; i++) {
+            // revisar si es el día actual
+            let isToday = i === currDate.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? 'active' : '';
+            tdTag.push([ { 
+                // key: `${months[currMonth]} ${i}`, 
+                key: `${i} ${currMonth + 1} ${currYear}`, 
+                className: isToday, 
+                value: i
+            } ]);
+        }
 
-    if(currentMonth < 0 || currentMonth > 11) {
-      let date = new Date(currentYear, currentMonth);
-      setDate(date);
-      setCurrentMonth(date.getFullYear());
-      setCurrentYear(date.getMonth());
-    } else {
-      setDate(new Date());
-    }
-  };
+        // ciclo para los primeros días del siguiente mes
+        for(let i = lastDayOfMonth; i < 6; i++) {
+            tdTag.push([ { 
+                // key: `${months[currMonth+1]} ${i - lastDayOfMonth + 1}`, 
+                key: `${i - lastDayOfMonth + 1} ${currMonth + 2} ${currYear}`,
+                className: "inactive", 
+                value: i - lastDayOfMonth + 1
+            } ]);
+        }
 
-  const handleNext = (event) => {
-    if((currentMonth + 1) < 0 || (currentMonth + 1) > 11) {
-      let date = new Date(currentYear, currentMonth);
-      setDate(date);
-      setCurrentMonth(date.getFullYear());
-      setCurrentYear(date.getMonth());
-    } else {
-      setDate(new Date());
-    }
-    setCurrentMonth( currentMonth + 1)
-  };
+        const separatedArrays = [];
+    
+        while (tdTag.length > 0) {
+            separatedArrays.push(tdTag.splice(0, 7));
+        }
 
-  const handlePrev = (event) => {
-    if((currentMonth - 1) < 0 || (currentMonth - 1) > 11) {
-      let date = new Date(currentYear, currentMonth);
-      setDate(date);
-      setCurrentMonth(date.getFullYear());
-      setCurrentYear(date.getMonth());
-    } else {
-      setDate(new Date());
-    }
-    setCurrentMonth( currentMonth - 1);
-  };
-
-  const handleDay = (event) => {
-    console.log(` ${event.target.textContent}`);
-  };
-
-  return (
-    <>
-      <div className='container'>
-        <header>
-          <div className='header'>
-            <p className='current-date'>{`${months[currentMonth]} ${currentYear}`}</p>
-            <button type="button" id="prev" className='btn btn-light' onClick={handlePrevNext}>prev</button>
-            <button type="button" id="next" className='btn btn-light' onClick={handlePrevNext}>next</button>
-          </div>
-        </header>
-
-        <table className=' calendar'>
-          <thead className='weeks'>
-              {/* d-flex justify-content-around */}
-            <tr className='row d-flex justify-content-around'>
-              { weekDays.map(day => (
-                <th scope="col">{ day }</th>
-              )) }
+        const daysOfMonth = separatedArrays.map((array, index) => (
+            <tr className='row border' key={index}>
+                { array.map((elemento, i) => (
+                    <>
+                        <div 
+                            key={ elemento[0].key } 
+                            data-index={ elemento[0].key }
+                            className={ `calendar-days ${elemento[0].className} col border-right p-0` }
+                            onDoubleClick={handleDay}
+                        >
+                            <div className='col'>
+                                <td
+                                    className='d-flex align-items-start justify-content-end'
+                                >
+                                    { elemento[0].value }
+                                </td>
+                                <div className=''>
+                                    { showEvent(elemento[0].key) }
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )) }
             </tr>
-          </thead>
-          <tbody className='days'>
-            
-            { renderCalendar() }
-            
-          </tbody>
-        </table>
+        ));
 
-      </div>
-    </>
-  );
+        return daysOfMonth;
+    };
+
+
+    const handlePrevNext = (event) => {
+        currMonth = event.target.id === "prev" ? currMonth - 1 : currMonth + 1;
+        // always have the actual current day
+        if(currMonth === new Date().getMonth() && currYear === new Date().getFullYear()) {
+            setCurrDate(new Date());
+        } else {
+            setCurrDate(new Date(currYear, currMonth));
+        }
+    };
+
+    const handleDay = (event) => {
+        // const time = prompt('Set the hour for the event:\nFormat: HH:mm');
+        const newAppointment = prompt('Event description:');
+        if(!newAppointment) return;
+        
+        console.log(newAppointment);
+        console.log(event.target.getAttribute('data-index'));
+        const newEvent = {
+            date: event.target.getAttribute('data-index'),
+            eventName: newAppointment
+        };
+        
+        setPersonalEvents([...personalEvents, {...newEvent}]);
+        let allEvents = JSON.stringify([...personalEvents, {...newEvent}]);
+        localStorage.setItem('event', allEvents);
+        
+        setEvents([...events, {...newEvent}]);
+        // console.log(newEvent);
+    };
+
+    const showEvent = (date) => {
+        let eventsArray = [];
+
+        events.map(event => {
+            if(!event.eventName) return;
+
+            if(date === event.date) {
+                eventsArray.push(
+                    <div className='event'>
+                        <p className='m-0'>{event.eventName}</p>
+                    </div>
+                );
+            }
+
+        });
+        return eventsArray;
+    }
+
+    return (
+        <>
+            <div className="">
+                <header className='header row mb-2 mt-5'>
+                    <div className="tittle col">
+                        <h4 className='current-date'>{`${months[currMonth]} ${currYear}`}</h4>
+                    </div>
+                    <div className='buttons col-3 d-flex justify-content-between'>
+                        <button type="button" id="prev" className='btn btn-light' onClick={handlePrevNext}>prev</button>
+                        <button type="button" id="next" className='btn btn-light' onClick={handlePrevNext}>next</button>
+                        <button type="button" id="today" className='btn btn-success' onClick={() => setCurrDate(new Date())}>today</button>
+                    </div>
+                </header>
+
+                <table className="calendar ">
+                    <thead className="calendar-header row mt-3 border-top border-left">
+                        { weekDays.map((day, i) => (
+                            <tr className="col border-right d-flex align-items-center justify-content-center">
+                                <th key={i} scope="col" >{ day }</th>
+                            </tr>
+                        )) }
+                    </thead>
+
+                    <tbody className="">
+                        { renderCalendar() }
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
 
 export default Calendar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Calendar2 from './components/Calendar2';
-import Test from './components/Test';
+import './styles/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Test /> */}
-    <Calendar2 />
+    <App />
   </React.StrictMode>,
 )
